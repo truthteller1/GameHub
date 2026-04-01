d9cd91457a4a14d4440e7b06c662be371247c5cc
chinmaya <chinshastry@gmail.com>
2026-04-01 18:23:14 +0000

    Implemented base game class structure and rudimentary GUI

---
+++ hub/game.py
@@ -0,0 +1,70 @@
+import pygame
+import sys
+white = (255,255,255)
+black = (0,0,0)
+navy = (1,3,43)
+red = (255, 0,0)
+orange = (255,165,0)
+
+pygame.init()
+
+SW = 1000
+SH = 800
+
+screen = pygame.display.set_mode((SW,SH))
+
+screen.fill(white)
+
+quit_rect = pygame.Rect(SW/2-40, SH-50 , 80, 40)
+
+font = pygame.font.SysFont(None, 30)
+font1 = pygame.font.SysFont("calibri", 50,bold=True)
+buttonfont = pygame.font.SysFont( None ,20)
+
+textsurf = buttonfont.render("QUIT", True, white)
+textrect = textsurf.get_rect()
+textrect.center = (SW/2, SH-30)
+
+gamesurf = font1.render("GameHub", True, white)
+gamerect = gamesurf.get_rect()
+gamerect.center = (SW/2 , 100)
+
+tttsurf = font.render("Tic-Tac-Toe",True,white)
+tttrect = tttsurf.get_rect()
+tttrect.center = (SW/2, SH/2 - 30)
+
+othellosurf = font.render("Othello",True,white)
+othellorect = othellosurf.get_rect()
+othellorect.center = (SW/2, SH/2 + 50)
+
+connect4surf = font.render("Connect4",True,white)
+connect4rect = connect4surf.get_rect()
+connect4rect.center = (SW/2, SH/2 + 130)
+
+tttcolor = pygame.Rect(SW/2 - 60, SH/2 -30 -25, 120, 50)
+othcolor = pygame.Rect(SW/2 - 60, SH/2 + 50 - 25, 120, 50)
+c4color = pygame.Rect(SW/2 - 60, SH/2 + 130 -25, 120, 50)
+
+while True:
+    for event in pygame.event.get():
+        if event.type ==pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                if quit_rect.collidepoint(event.pos):
+                    pygame.quit()
+                    sys.exit()
+                
+    screen.fill(navy)
+    x = int(quit_rect.height/2)
+    pygame.draw.rect(screen, red, quit_rect, border_radius = x)
+    pygame.draw.rect(screen, orange, tttcolor, border_radius = int(tttcolor.height/2))
+    pygame.draw.rect(screen, orange, othcolor, border_radius = int(othcolor.height/2))
+    pygame.draw.rect(screen, orange, c4color, border_radius = int(c4color.height/2))
+    screen.blit(gamesurf, gamerect)
+    screen.blit(textsurf, textrect)
+    screen.blit(tttsurf, tttrect)
+    screen.blit(othellosurf,othellorect)
+    screen.blit(connect4surf, connect4rect)
+    pygame.display.flip()
---
+++ hub/games/game_class.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+class Game:
+    def __init__(self, p1, p2, starter, board):
+        self.p1 = p1
+        self.p2 = p2
+        self.turn = starter
+        self.gameboard = np.zeros(board)
+
+    def switch_turn(self):
+        self.turn = self.p1 if (self.turn == self.p2) else self.p2
+
+    def make_move(self, move):
+        if (self.turn == self.p1):
+            self.gameboard[move] = 1
+        else:
+            self.gameboard[move]  = 2
+        
+    def check_win_condition(self):
+        pass
+
