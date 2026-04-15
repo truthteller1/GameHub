479c7531329892c578247a3197b7c1d23786185a
Chinmaya Shastry <chinshastry@gmail.com>
2026-04-15 23:52:30 +0530

    Partially implemented button class for more concise code

--- hub/game.py
+++ hub/game.py
@@ -1,6 +1,9 @@
 import pygame
 import sys
 from games.tictactoe import TicTacToe
+from games.othello import Othello
+import csv
+
 white = (255,255,255)
 black = (0,0,0)
 navy = (1,3,43)
@@ -16,6 +19,44 @@ screen = pygame.display.set_mode((SW,SH))
 
 screen.fill(white)
 
+def create_textsurf(text,size,font="calibri",text_rgb=white,bg_rgb=None):
+    text_font = pygame.font.SysFont(font,size)
+    text_surf = text_font.render(text,True,text_rgb, bg_rgb)
+    return text_surf
+
+def create_button(text, size, font, text_rgb=white,bg_rgb=None):
+    surf = create_textsurf(text, size, font, text_rgb,bg_rgb)
+
+class Button:
+    #type is true for text
+    def __init__(self,type,location,action,text=None,fontsize=None,font="calibri",text_color=white,bg_color=None,border_color=white,img=None, size = (120,50)):
+        if type:
+            self.type = "text"
+            self.text_surf = create_textsurf(text,fontsize,font,text_color,bg_color)
+            self.text_rect = self.text_surf.get_rect(center = self.location)
+            self.rect = pygame.Rect(location[0]-size[0]/2,location[1]-size[1]/2,size[0],size[1])
+            
+        else:
+            self.type = "img"
+            self.img = pygame.image.load(img).convert_alpha()
+            self.rect = self.img.get_rect(center=self.location)
+        self.action = action
+        self.location = location
+        self.bg_color = bg_color
+        self.border_color = border_color
+        self.size = size
+    
+    def render(self):
+        if self.type == "img":
+            screen.blit(self.img,self.location)
+        else:
+            pygame.draw.rect(screen,self.bg_color,self.rect,5,(self.rect.height//2))
+            screen.blit(self.text_surf,self.text_rect)
+    
+    def onclick(self,pos):
+        if pygame.mouse.get_pressed()[0] and self.rect.collidepoint(pygame.mouse.get_pos()):
+            self.action()
+            
 quit_rect = pygame.Rect(SW/2-40, SH-50 , 80, 40)
 
 font = pygame.font.SysFont(None, 30)
@@ -45,6 +86,12 @@ connect4rect.center = (SW/2, SH/2 + 130)
 tttcolor = pygame.Rect(SW/2 - 60, SH/2 -30 -25, 120, 50)
 othcolor = pygame.Rect(SW/2 - 60, SH/2 + 50 - 25, 120, 50)
 c4color = pygame.Rect(SW/2 - 60, SH/2 + 130 -25, 120, 50)
+buttons = [
+    #Button(True,(SW/2, SH/2 - 30),)
+]
+gamebuttons= [
+
+]
 
 game = None
 
@@ -61,8 +108,10 @@ while True:
                         sys.exit()
                     else:
                         game = False
-                elif tttrect.collidepoint(event.pos):
+                elif tttcolor.collidepoint(event.pos):
                     game = TicTacToe("me","ching","me",(10,10),screen)
+                elif othcolor.collidepoint(event.pos):
+                    game = Othello("me","ching","me",(8,8),screen)
                 if game:
                     game.checkpress(event)
                 
@@ -79,7 +128,11 @@ while True:
         screen.blit(tttsurf, tttrect)
         screen.blit(othellosurf,othellorect)
         screen.blit(connect4surf, connect4rect)
+        for b in buttons:
+            b.render()
     else:
         game.renderboard("fah")
+        for b in gamebuttons:
+            b.render()
         pass
     pygame.display.flip()
