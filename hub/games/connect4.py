c65d753d86504d24a8779be56027075154d7e0c3
Arnav <arnavs1289@gmail.com>
2026-04-15 18:18:35 +0000

    Implemented connect4 along with win condition

---
+++ hub/games/connect4.py
@@ -0,0 +1,76 @@
+from .game_class import Game
+import pygame
+import numpy as np
+
+class Connect4(Game):
+    def __init__(self, p1, p2, starter, board, screen):
+        super().__init__(p1,p2,starter,board)
+        self.screen = screen
+        self.boardrects = []
+        #restructure vars
+        bx = 20
+        by = 20
+        buffer  = 10
+        side = 30
+        for i in range(self.gameboard.shape[0]):
+            self.boardrects.append([])
+            for j in range(self.gameboard.shape[1]):
+                self.boardrects[i].append(pygame.Rect((bx + j*(buffer+side),by+i*(buffer+side),side,side)))
+
+    def renderboard(self, dimensions):
+        bx = 20
+        by = 20
+        buffer  = 10
+        side = 30
+        for i in range(self.gameboard.shape[0]):
+            #self.boardrects.append([])
+            for j in range(self.gameboard.shape[1]):
+                #self.boardrects[i].append(pygame.Rect((bx + j*(buffer+side),by+i*(buffer+side),side,side)))
+                pygame.draw.rect(self.screen, (0,255,0), self.boardrects[i][j])
+                if self.gameboard[i][j] == 1:
+                    #REPLACE THIS WITH X
+                    pygame.draw.circle(self.screen,(255,0,0),(bx + j*(buffer+side)+side//2,by+i*(buffer+side)+side/2),side//2)
+                elif self.gameboard[i][j] == 2:
+                    #REPLACE WITH O
+                    pygame.draw.circle(self.screen,(0,0,255),(bx + j*(buffer+side)+side//2,by+i*(buffer+side)+side//2),side//2)
+
+    def make_move(self,move):
+        if self.turn == self.p1:
+            self.gameboard[np.argwhere(self.gameboard[:,move[1]] == 0)[-1],move[1]] = 1
+        else:
+            self.gameboard[np.argwhere(self.gameboard[:,move[1]] == 0)[-1],move[1]] = 2
+        print(self.gameboard)
+
+    def checkpress(self,click):
+        for i in range(self.gameboard.shape[0]):
+            for j in range(self.gameboard.shape[1]):
+                if (self.boardrects[i][j]).collidepoint(click.pos):
+                    self.make_move((i,j))
+                    if self.check_win_condition((i,j)) != 0:
+                        return True
+                    self.switch_turn()
+
+    def check_win_condition(self, move):
+        vert = self.gameboard[:,move[1]]
+        hor = self.gameboard[move[0],:]
+        rdiag = self.gameboard.diagonal(move[1] - move[0])
+        ldiag = self.gameboard[:,::-1].diagonal(self.gameboard.shape[0] - 1 - move[0] - move[1])
+
+        win1_vert= ((vert[:-3] == vert[1:-2]) * (vert[:-3] == vert[2:-1]) * (vert[:-3] == vert[3:]) * (vert[:-3] == 1)).any()
+        win1_hor= ((hor[:-3] == hor[1:-2]) * (hor[:-3] == hor[2:-1]) * (hor[:-3] == hor[3:]) * (hor[:-3] == 1)).any()
+        win1_rdiag= ((rdiag[:-3] == rdiag[1:-2]) * (rdiag[:-3] == rdiag[2:-1]) * (rdiag[:-3] == rdiag[3:]) * (rdiag[:-3] == 1)).any()
+        win1_ldiag= ((ldiag[:-3] == ldiag[1:-2]) * (ldiag[:-3] == ldiag[2:-1]) * (ldiag[:-3] == ldiag[3:]) * (ldiag[:-3] == 1)).any()
+
+        win2_vert= ((vert[:-3] == vert[1:-2]) * (vert[:-3] == vert[2:-1]) * (vert[:-3] == vert[3:]) * (vert[:-3] == 2)).any()
+        win2_hor= ((hor[:-3] == hor[1:-2]) * (hor[:-3] == hor[2:-1]) * (hor[:-3] == hor[3:]) * (hor[:-3] == 2)).any()
+        win2_rdiag= ((rdiag[:-3] == rdiag[1:-2]) * (rdiag[:-3] == rdiag[2:-1]) * (rdiag[:-3] == rdiag[3:]) * (rdiag[:-3] == 2)).any()
+        win2_ldiag= ((ldiag[:-3] == ldiag[1:-2]) * (ldiag[:-3] == ldiag[2:-1]) * (ldiag[:-3] == ldiag[3:]) * (ldiag[:-3] == 2)).any()
+
+        if win1_vert or win1_hor or win1_rdiag or win1_ldiag:
+            return 1
+        elif win2_vert or win2_hor or win2_rdiag or win2_ldiag:
+            return 2
+        elif not (self.gameboard == 0).any():
+            return -1
+        else:
+            return 0
